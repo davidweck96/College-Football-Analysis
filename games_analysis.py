75e4ce52605c573398d55287781db0362c17feb1
David Weck <davidweck96@gmail.com>
2022-11-08 19:57:34 -0500

    created game results dataframe containing results 2012-2021

--- games_analysis.py
+++ games_analysis.py
@@ -12,8 +12,36 @@ api_config = cfbd.ApiClient(config)
 
 #Connecting to games API and getting game data
 games_api = cfbd.GamesApi(api_config)
-game_results_temp= games_api.get_games(year = 2021)
-team_game_stats_temp = games_api.get_team_game_stats(year = 2021, week = 2)
+game_results_df = pd.DataFrame()
+
+for i in range(2012, 2022):
+    game_results_temp = games_api.get_games(year = i)
+    game_results_df_temp = pd.DataFrame.from_records([dict(game_id = game.id \
+                                                          , season = game.season \
+                                                          , week = game.week \
+                                                          , conference_game = game.conference_game \
+                                                          , excitement_index = game.excitement_index \
+                                                          , attendance = game.attendance  \
+                                                          , neutral_site = game.neutral_site \
+                                                          , away_conference = game.away_conference \
+                                                          , away_division = game.away_division \
+                                                          , away_id = game.away_id \
+                                                          , away_points = game.away_points \
+                                                          , away_post_win_prob = game.away_post_win_prob \
+                                                          , away_postgame_elo = game.away_postgame_elo \
+                                                          , away_pregame_elo = game.away_pregame_elo \
+                                                          , away_team = game.away_team \
+                                                          , home_conference = game.home_conference \
+                                                          , home_division = game.home_division \
+                                                          , home_id = game.home_id \
+                                                          , home_points = game.home_points \
+                                                          , home_post_win_prob = game.home_post_win_prob \
+                                                          , home_postgame_elo = game.home_postgame_elo \
+                                                          , home_pregame_elo = game.home_pregame_elo \
+                                                          , home_team = game.home_team) \
+                                                          for game in game_results_temp])
+    game_results_df = pd.concat([game_results_df, game_results_df_temp.dropna(axis=0)], axis=0)
+    
 
 #Connecting to advanced stats API and getting games stats
 stats_api = cfbd.StatsApi(api_config)
